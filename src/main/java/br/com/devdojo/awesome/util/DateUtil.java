39257f5d64542e1b1d077503d4e5e63c20e80671
Milton-Viegas <milton.viegas.br@gmail.com>
2023-03-05 16:23:50 +0000

    moved ApplicationStart from start

--- src/main/java/br/com/devdojo/awesome/util/DateUtil.java
+++ src/main/java/br/com/devdojo/awesome/util/DateUtil.java
@@ -1,10 +1,16 @@
 package br.com.devdojo.awesome.util;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@Component
 public class DateUtil {
     public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime) {
-        return DateTimeFormatter
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:MM:ss").format(localDateTime);
+
     }
 }
